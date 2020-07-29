b4f0533064ee458f15f8f90bbe720718eaa478b6
suhyeon-ju <jusu2529@gmail.com>
2020-07-29 21:25:02 +0900

    #Reusable Components with JSX+Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function Food({fav}){   //props.fav 는 {fav}랑 같다
+  return <h1>I like {fav}</h1>;
+}
+function App() {  //component : html을 반환하는 함수
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+           Hello!!
+           <Food fav="kimchi"/>
+           <Food fav="ramen"/>
+           <Food fav="samgiopsal"/>
+           <Food fav="chukumi"/>
+        </h1>
     </div>
+      
+   
   );
 }
 
