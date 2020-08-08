d6ed2fdcec1bc0c1286f444e7be2648e3435ad6b
suhyeon-ju <jusu2529@gmail.com>
2020-08-08 11:20:22 +0900

    Updates

---
+++ precache-manifest.f6a817a3f63d419b27f6fc7a3c25c94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561765e61b495e23b3d51d6b720147cc",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "43fefbff0f74916c5eea",
+    "url": "/movie_app_2020/static/css/main.4ce791b3.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fefbff0f74916c5eea",
+    "url": "/movie_app_2020/static/js/main.14c07bfa.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
