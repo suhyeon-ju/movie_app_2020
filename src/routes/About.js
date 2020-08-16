d4da08689027d38bce2483499c85382734bb69a7
suhyeon-ju <jusu2529@gmail.com>
2020-08-16 12:33:53 +0900

    detail page start

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 // import { func } from "prop-types";
 
-function About(){
-  return <span>About this page: I built it because i love movies.</span>;
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        "Freedom is the freedom to say that two plus two make four. If that i granted, all else follows."
+       </span>
+      <span>-George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
